4c13661bba8a6ea434daf2d8da584ce8c4e822dd
kaunda-a <kaunda-a@users.noreply.github.com>
2025-07-18 17:24:58 +0200

    clerk removed

--- src/features/profile/components/profile-view-page.tsx
+++ src/features/profile/components/profile-view-page.tsx
@@ -1,9 +1,7 @@
-import { UserProfile } from '@clerk/nextjs';
-
 export default function ProfileViewPage() {
   return (
     <div className='flex w-full flex-col p-4'>
-      <UserProfile />
+      <div>Profile Page</div>
     </div>
   );
 }
