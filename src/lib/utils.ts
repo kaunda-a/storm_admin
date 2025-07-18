2be140db8c17deadece1671990a0acca7a511f6f
kaunda-a <kaunda-a@users.noreply.github.com>
2025-07-18 19:28:26 +0200

    flaw

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,13 +11,13 @@ export function formatBytes(
     sizeType?: 'accurate' | 'normal';
   } = {}
 ) {
-  const { decimals = 0, sizeType = 'normal' } = opts;
+  const { sizeType = 'normal' } = opts;
 
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
   const accurateSizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB'];
   if (bytes === 0) return '0 Byte';
   const i = Math.floor(Math.log(bytes) / Math.log(1024));
-  return `${(bytes / Math.pow(1024, i)).toFixed(decimals)} ${
+  return `${(bytes / Math.pow(1024, i)).toFixed(0)} ${
     sizeType === 'accurate'
       ? (accurateSizes[i] ?? 'Bytest')
       : (sizes[i] ?? 'Bytes')
