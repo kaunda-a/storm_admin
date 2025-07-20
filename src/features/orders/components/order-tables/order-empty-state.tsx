c80c79f6eb5961a367408cdeb87614a537843b23
kaunda-a <kaunda-a@users.noreply.github.com>
2025-07-20 15:49:40 +0200

    incompatible

--- src/features/orders/components/order-tables/order-empty-state.tsx
+++ src/features/orders/components/order-tables/order-empty-state.tsx
@@ -12,9 +12,11 @@ export function OrdersEmptyState() {
       title="No orders found"
       description="There are no orders to display. You can create a new order by clicking the button below."
       action={
-        <Link href="/dashboard/orders/new">
-          Create New Order
-        </Link>
+        <Button size='sm' className='relative' asChild>
+          <Link href="/dashboard/orders/new">
+            Create New Order
+          </Link>
+        </Button>
       }
     />
   )
